25a2b3d911a529dd6c9c193dce2fe623e09ede66
KasiMKurtay <kasm.krt44@gmail.com>
2025-03-08 22:51:03 +0300

    First Init

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import express from "express"; // Express.js framework'ünü içe aktarır.
+import dotenv from "dotenv"; // Çevresel değişkenleri (ENV) kullanmak için dotenv'i içe aktarır.
+import authRoutes from "./routes/auth.route.js" // Kimlik doğrulama rotalarını içe aktarır.
+import { connectDB } from "./lib/db.js"; // MongoDB bağlantı fonksiyonunu içe aktarır.
+
+dotenv.config(); // `.env` dosyasındaki değişkenleri kullanabilmek için dotenv'i çalıştırır.
+
+const app = express(); // Express uygulamasını oluşturur.
+
+const PORT = process.env.PORT; // `.env` dosyasından gelen PORT değişkenini alır.
+
+app.use(express.json()); // Gelen isteklerde JSON verilerini işleyebilmek için middleware ekler.
+
+app.use("/api/auth", authRoutes); 
+// Tüm kimlik doğrulama ile ilgili rotaları "/api/auth" altında kullanılabilir hale getirir.
+
+app.listen(PORT, () => { 
+  console.log("Server is running on PORT " + PORT); 
+  connectDB(); // Sunucu başladıktan sonra MongoDB bağlantısını başlatır.
+});
